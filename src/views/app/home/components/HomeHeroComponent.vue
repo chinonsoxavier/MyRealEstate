1328afd4b30452a2fc41dab391b520d053c1e828
chinonsoxavier <101524269+chinonsoxavier@users.noreply.github.com>
2024-12-02 12:58:37 +0100

    Update HomeHeroComponent.vue

--- src/views/app/home/components/HomeHeroComponent.vue
+++ src/views/app/home/components/HomeHeroComponent.vue
@@ -3,7 +3,7 @@
 <template>
     <div >
          <div class="text-center max-w-[620px] mobile:[400px] w-full" >
-            <p class="text-5xl  lg:text-6xl  font-medium" >Get the ideal home for your family</p>
+            <p class="text-5xl leading-[67px] lg:text-6xl  font-medium" >Get the ideal home for your family</p>
         </div>
         <div class="text-center mt-12" >
             <p class="text-lightText font-medium mobile-text-base text-lg" >We’ve more than 745,000 apartments, place & plot.</p>
